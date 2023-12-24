8d7eb7d32f7596250b4f1d53b68bd2440c59d8a5
ahmed hussien <133219145+ahmedhusseinDBS@users.noreply.github.com>
2023-12-24 12:13:17 +0200

    fierst push

---
+++ AllinOnApp/AllinOnAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllinOnAppApp.swift
+//  AllinOnApp
+//
+//  Created by ahmed hussien on 24/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct AllinOnAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AllinOnApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AllinOnApp
+//
+//  Created by ahmed hussien on 24/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
